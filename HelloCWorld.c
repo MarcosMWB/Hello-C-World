183205a5a773423ebe59f8dc82446e8224fc12a7
MarcosMWB <marcosmwb1919@gmail.com>
2018-03-07 16:27:39 -0300

    Display: Hello C World! Hello CPP World!

--- HelloCWorld.c
+++ HelloCWorld.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-void helloworld(char var){
-  printf("Hello %c World!", var);
+void helloworld(char var,char x){
+  
+    if(x == ' ')
+        printf("Hello %c World!\n", var);
+    else
+        printf("Hello %c%c%c World!\n", var, x, x);
+  
 }
 
 int main(){
 
-  helloworld(C);
-  helloworld(CPP);
+  helloworld('C',' ');
+  helloworld('C','P');
   
 return 0;
+  
 }
